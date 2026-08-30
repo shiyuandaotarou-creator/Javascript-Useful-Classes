cc1f7aa555d0e85d3924af7e3bcc5f104ffc911c
shiyuandaotarou-creator <shiyuandaotarou@gmail.com>
2026-08-30 20:13:28 +0900

    Add mouse position tracking to Input class

--- misc/Input.js
+++ misc/Input.js
@@ -1,7 +1,7 @@
 import Help from 'https://cdn.jsdelivr.net/gh/shiyuandaotarou-creator/Javascript-Useful-Classes@main/misc/Help.js';
-const h = new Help("Input", "キーインプット配列を返します");
+const h = new Help("Input", "キー入力やマウス座標を取得できます");
 export default class Input {
-    constructor(funcPressed, funcReleased, parent = undefined) {
+    constructor(funcPressed, funcReleased, parent = window) {
         this._pressedKeys = {};
         this.registerKey = (e) => {
             this._pressedKeys[e.code] = true;
@@ -9,16 +9,22 @@ export default class Input {
         this.releaseKey = (e) => {
             this._pressedKeys[e.code] = false;
         }
+        this.giveMousePos = (e) => {
+            this._mousePos.x = e.clientX;
+            this._mousePos.y = e.clientY;
+        }
+        this._mousePos = {};
         this.funcPressed = funcPressed;
         this.funcReleased = funcReleased;
         window.addEventListener("blur", () => {
             this._pressedKeys = {};
         })
-        h.m("Input(funcPressed,funcReleased) ", "初期化", ["funcPressed:任意のキーを押したときに発動する関数", "funcReleased:任意のキーを離した瞬間に発動する関数", "parent=undefined:クリックのparent"])
+        h.m("Input(funcPressed,funcReleased) ", "初期化", ["funcPressed:任意のキーを押したときに発動する関数", "funcReleased:任意のキーを離した瞬間に発動する関数", "parent=window:クリックのparent"])
         h.m("start(parent) ", "キー入力受付を開始", ["parent:反応するエリア"])
         h.m("end()", "キー入力受付を終了", ["引数なし"])
         h.m("isPressed(keycode) ", "キー入力があるか判定", ["keycode:キーコード。KeySやKeyEのString形式で渡す。"])
-        h.g("pressedKeys", "現在押されているキーのオブジェくト。'key':boolean型のデータ")
+        h.g("pressedKeys", "現在押されているキーのオブジェクト。'key':boolean型のデータ")
+        h.g("mousePos", "マウスカーソルの座標")
         if (parent) {
             this.parent = parent;
             this.start(parent)
@@ -26,6 +32,9 @@ export default class Input {
     }
     help() {
         h.helpShow();
+    }
+    get mousePos() {
+        return this._mousePos;
     }
     get pressedKeys() {
         return this._pressedKeys;
@@ -36,16 +45,18 @@ export default class Input {
      */
     start(parent) {
         this.parent = parent;
-        this.parent.addEventListener("keydown", this.registerKey)
-        this.parent.addEventListener("keydown", this.funcPressed)
-        this.parent.addEventListener("keyup", this.releaseKey)
-        this.parent.addEventListener("keyup", this.funcReleased)
+        this.parent.addEventListener("keydown", this.registerKey);
+        this.parent.addEventListener("keydown", this.funcPressed);
+        this.parent.addEventListener("keyup", this.releaseKey);
+        this.parent.addEventListener("keyup", this.funcReleased);
+        this.parent.addEventListener("mousemove", this.giveMousePos)
     }
     end() {
         this.parent.removeEventListener("keydown", this.registerKey);
         this.parent.removeEventListener("keydown", this.funcPressed)
         this.parent.removeEventListener("keyup", this.releaseKey);
         this.parent.removeEventListener("keyup", this.funcReleased)
+        this.parent.removeEventListener("mousemove", this.giveMousePos)
     }
     isPressed(code) {
         if (this._pressedKeys[code]) {
