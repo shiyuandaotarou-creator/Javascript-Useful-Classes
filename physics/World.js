508c59eadfa846238b55719a8f1c5e9534e4ec59
shiyuandaotarou-creator <shiyuandaotarou@gmail.com>
2026-08-31 01:58:31 +0900

    Add files via upload

---
+++ physics/World.js
@@ -0,0 +1,15 @@
+export default class World {
+    constructor(width = 500, height = 500, canvasId = "canvas") {
+        /** @type { HTMLCanvasElement } */
+        this.canvas = document.getElementById(canvasId);
+        this.rectList = [];
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.ctx = this.canvas.getContext("2d")
+    }
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+    }
+}
