7eda1d50130ea4e57e9ddf57718579387f8c2554
shiyuandaotarou-creator <shiyuandaotarou@gmail.com>
2026-08-31 15:16:13 +0900

    Integrate Help class for World documentation
    Added Help class integration for documentation and usage guidance.

--- physics/World.js
+++ physics/World.js
@@ -1,3 +1,4 @@
+import Help from 'https://cdn.jsdelivr.net/gh/shiyuandaotarou-creator/Javascript-Useful-Classes@main/misc/Help.js';
 export default class World {
     constructor(width = 500, height = 500, canvasId = "canvas") {
         /** @type { HTMLCanvasElement } */
@@ -9,8 +10,20 @@ export default class World {
         this.canvas.width = width;
         this.canvas.height = height;
         this.ctx = this.canvas.getContext("2d")
+        this.h = new Help("World", "キャンバス統一データを扱います");
+        this.h.m("World(width,height,canvasId)", "初期化", ["width:横解像度", "height:縦解像度", "canvasId:キャンバスID"]);
+        this.h.m("clearCanvas()", "キャンバスを初期化", ["引数なし"]);
+        this.h.g("canvasId", "キャンバスのID");
+        this.h.g("canvasHeight", "キャンバスの縦解像度")
+        this.h.g("canvasWidth", "キャンバスの横解像度")
+        this.h.g("canvas", "キャンバスエレメント")
+        this.h.g("ctx", "2Dキャンバスコンテキスト")
+        this.h.sg("rectList", "登録されているRectの一覧")
+    }
+    help() {
+        this.h.helpShow();
     }
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
     }
-}
+} 
